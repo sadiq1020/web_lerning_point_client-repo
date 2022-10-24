5cf82c84ef110567b47cee8551530de17784d024
sadiq1020 <sajidnsubd@gmail.comm>
2022-10-24 21:41:08 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC5IjUBtvRsmalEbl0E_93pAKYPV1Fhyag",
+    authDomain: "assignment-ten-f3799.firebaseapp.com",
+    projectId: "assignment-ten-f3799",
+    storageBucket: "assignment-ten-f3799.appspot.com",
+    messagingSenderId: "490959895975",
+    appId: "1:490959895975:web:78724bafdd3b2fcbdb8a82"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
